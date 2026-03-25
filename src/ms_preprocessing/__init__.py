86a232f301c89312fe6cee5007b1f0f564a80fdd
bosschen0429 <j25057175@gmail.com>
2026-03-25 13:03:47 +0800

    build: bump version to 1.2.1

--- src/ms_preprocessing/__init__.py
+++ src/ms_preprocessing/__init__.py
@@ -6,7 +6,7 @@ from .bootstrap_paths import ensure_ms_core_src_on_path
 
 ensure_ms_core_src_on_path(Path(__file__).resolve())
 
-__version__ = "1.2.0"
+__version__ = "1.2.1"
 __author__ = "Your Name"
 
 from ms_preprocessing.utils.results import ProcessingMetadata, ProcessingResult
